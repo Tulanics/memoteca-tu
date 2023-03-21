bb679dee877d2a0bf7c376fc62acdd22d93459ec
Tulani Silva <tulani.santos@raizen.com>
2023-03-21 10:30:34 -0300

    cabecalho da memoteca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+@Component({//Decorator
+  selector: 'app-root',// será usado na forma de tags pro conteúdo poder ser renderizado na tela
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
-@NgModule({
+@NgModule({ //decorator
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//classes de serviços
+  bootstrap: [AppComponent] // indica qual componente será primeiramente renderizado na aplicação
 })
-export class AppModule { }
+export class AppModule { } /*componentes, diretivas e pipes que se quer exortar para fora do modulo, para que
+sejam visíveis por outros módulos*/
